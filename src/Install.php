971d2432590d28be4052fb35cc7ce5baa4c46236
rocky <631670747@qq.com>
2022-11-02 13:05:20 +0800

    init

--- src/Install.php
+++ src/Install.php
@@ -11,21 +11,6 @@ class Install
         
         $filesystem = new \Symfony\Component\Filesystem\Filesystem();
         $filesystem->mirror(dirname(__DIR__, 1) . '/config', config_path());
-        
-        //静态
-        $file = public_path('exadmin').DIRECTORY_SEPARATOR.'version';
-        $update = false;
-        if(!is_file($file)){
-            $update = true;
-        }
-        if(!$update && file_get_contents($file) != ex_admin_version()){
-            $update = true;
-        }
-        if($update){
-            $filesystem = new Filesystem();
-            $filesystem->mirror(dirname(__DIR__,2) . '/ex-admin-ui/resources',public_path('exadmin'),null,['override'=>true]);
-            file_put_contents($file,ex_admin_version());
-        }
     }
 
     /**
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -12,10 +12,25 @@ class ServiceProvider implements Bootstrap
      * @return void
      */
     public static function start($worker){
+        self::updateVersion();
         $config = admin_config('admin');
         $config['plugin']['namespace'] = 'addons';
         $config['plugin']['dir'] = base_path('addons');
         admin_config($config,'admin');
         \ExAdmin\ui\support\Container::getInstance()->plugin->register();
+    }
+    protected static function updateVersion(){
+        $file = public_path('exadmin').DIRECTORY_SEPARATOR.'version';
+        $update = false;
+        if(!is_file($file)){
+            $update = true;
+        }
+        if(!$update && file_get_contents($file) != ex_admin_version()){
+            $update = true;
+        }
+        if($update){
+            app(\Illuminate\Contracts\Console\Kernel::class)->call('vendor:publish',['--force'=>true,'--tag'=>['ex-admin-ui']]);
+            file_put_contents($file,ex_admin_version());
+        }
     }
 }
